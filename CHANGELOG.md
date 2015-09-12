14d95c843f69bdf540e0bb0a4daadd65606ca92a
Jonathan Hartman <j@p4nt5.com>
2015-09-11 23:21:37 -0700

    Initial commit; a freshly generated cookbook

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+X2go Client Cookbook CHANGELOG
+==============================
+
+v0.0.1 (2015-09-11)
+-------------------
+- Development started
---
+++ metadata.rb
@@ -0,0 +1,11 @@
+# Encoding: UTF-8
+#
+# rubocop:disable SingleSpaceBeforeFirstArg
+name             'x2go-client'
+maintainer       'Jonathan Hartman'
+maintainer_email 'jonathan.hartman@socrata.com'
+license          'apache2'
+description      'Installs/Configures x2go-client'
+long_description 'Installs/Configures x2go-client'
+version          '0.0.1'
+# rubocop:enable SingleSpaceBeforeFirstArg
